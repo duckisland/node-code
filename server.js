7fc57004862435b88b49347658e0078222fbed8a
Your Name <you@example.com>
2018-10-26 21:30:40 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+// Handlebars JS (hbs)
+const hbs = require('hbs')
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+// Tell Express to use hbs as the view engine
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next)=> {
+  var now = new Date().toString()
+
+  console.log(`${now}: ${req.method} ${req.url}`)
+  next()
+})
+
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('allCaps', (text)=> {
+    return text.toUpperCase()
+})
+
+var person = {
+  firstName: 'Kevin',
+  lastName: 'Barrett',
+  favoriteColor: 'Blue',
+  toys: ['Hotwheels parking garage','Foldhaven','Pinball']
+}
+app.get('/',(req, res) => {
+  //response.send(person)
+  res.render('index.hbs', {
+    pageTitle: 'Index Page',
+    person})
+})
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{
+      pageTitle: 'About Page',
+      person
+    })
+})
+
+app.get('/bad', (req,res) => {
+  res.send({
+    response: 'Bad',
+    error: 'Not good'
+  })
+})
+app.listen(81, () => {
+  console.log('App is running and such...')
+})
